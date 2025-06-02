bd78a9f14f4fd4f712907e488c2e600a9cf6cdb6
Mohammed Almaqbali <116481530+almaqbalim@users.noreply.github.com>
2025-06-03 01:01:08 +0400

    updated streamlit app

--- Bikeshare_Analysis.py
+++ Bikeshare_Analysis.py
@@ -25,8 +25,8 @@ user_colors = {
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv(
-        r"C:\Users\engal\OneDrive\Desktop\CaseStudy\BikeShire\Dashboard\Data\dataset_after_clean.csv")
+    url = "https://huggingface.co/datasets/almaqbalim/BikeShare/resolve/main/dataset.csv"
+    df = pd.read_csv(url)
     df['started_at'] = pd.to_datetime(df['started_at'])
     df['ended_at'] = pd.to_datetime(df['ended_at'])
     df['ride_length'] = (df['ended_at'] - df['started_at']
